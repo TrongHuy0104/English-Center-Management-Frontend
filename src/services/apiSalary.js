d27fb4f40751555ae280694d491c54eb39d65cb6
congxdanh <danh.hsb13@gmail.com>
2024-10-24 00:54:41 +0700

    clean code

---
+++ src/services/apiSalary.js
@@ -0,0 +1,67 @@
+import axios from "../utils/axios";
+export async function getAllSalaries() {
+  try {
+    const res = await axios.get("/salaries", { withCredentials: true });
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getSalary(id) {
+  try {
+    const res = await axios.get(`/salaries/${id}`, { withCredentials: true });
+    console.log("res1", res);
+
+    return res;
+  } catch (error) {
+    console.error("Get Salary by ID error:", error);
+  }
+}
+
+export async function CreateSalary(formData) {
+  try {
+    const res = await axios.post("/salaries", formData, {
+      withCredentials: true,
+    });
+    console.log("Response from server:", res);
+    return res;
+  } catch (error) {
+    if (error.response) {
+      // Server trả về response với status code khác 2xx
+      console.error("Server responded with an error:", error.response.data);
+    } else if (error.request) {
+      // Yêu cầu đã được gửi nhưng không nhận được phản hồi
+      console.error("No response received from server:", error.request);
+    } else {
+      // Một lỗi xảy ra trong quá trình thiết lập yêu cầu
+      console.error("Error in setting up the request:", error.message);
+    }
+    throw error;
+  }
+}
+
+export async function deleteSalary(id) {
+  try {
+    const res = await axios.delete(`/salaries/${id}`, {
+      withCredentials: true,
+    });
+
+    return res;
+  } catch (error) {
+    console.error("Delete error:", error);
+  }
+}
+
+export async function updateSalary(id, updatedData) {
+  try {
+    const res = await axios.patch(`/salaries/${id}`, updatedData, {
+      withCredentials: true,
+    });
+
+    return res;
+  } catch (error) {
+    console.error("Update error:", error.response || error.message);
+    throw error; // Ném lỗi để có thể xử lý lỗi tại nơi gọi hàm này
+  }
+}
