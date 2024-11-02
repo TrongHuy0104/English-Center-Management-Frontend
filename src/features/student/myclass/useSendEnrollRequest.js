4cbb1d8ba9fb8edafd35cdd61f611fcea977bf3f
huy01040104 <tronghuy01040@gmail.com>
2024-11-02 18:28:48 +0700

    student: fix send request enroll class

--- src/features/student/myclass/useSendEnrollRequest.js
+++ src/features/student/myclass/useSendEnrollRequest.js
@@ -3,17 +3,18 @@ import { sendEnrollRequest } from "../../../services/apiStudent";
 import { toast } from "react-hot-toast";
 
 export default function useSendEnrollRequest() {
-  const mutation = useMutation({
-    mutationFn: sendEnrollRequest(),
-    onSuccess: () => {
-      toast.success("Enroll request sent successfully!");
-    },
-    onError: (error) => {
-      toast.error(
-        error?.response?.data?.message || "Failed to send enroll request."
-      );
-    },
-  });
+    const mutation = useMutation({
+        mutationFn: sendEnrollRequest,
+        onSuccess: () => {
+            toast.success("Enroll request sent successfully!");
+        },
+        onError: (error) => {
+            toast.error(
+                error?.response?.data?.message ||
+                    "Failed to send enroll request."
+            );
+        },
+    });
 
-  return mutation;
+    return mutation;
 }
